c95cc34af32dad5c0576bef7e81bf9c0b1e4456d
Brian Larson <brianl@scgpr.com>
2020-08-18 22:34:48 -0500

    trying to set up button pressed listeners with queue mgmmt but there's no need to?

--- app.js
+++ app.js
@@ -4,19 +4,40 @@
     // use first and only elevator
     const elevator = elevators[0];
 
+    // set up listeners for elevator calls on all floors
+    for (let i = 0; i < floors.length; i++) {
+      floors[i].on("up_button_pressed", function () {
+        if (!elevator.destinationQueue.includes(i)) {
+          elevator.destinationQueue.push(i);
+        }
+      });
+      floors[i].on("down_button_pressed", function () {
+        if (!elevator.destinationQueue.includes(i)) {
+          elevator.destinationQueue.push(i);
+        }
+      });
+    }
+
     // when a passenger pushes a floor button ...
     elevator.on("floor_button_pressed", function(floorNum) {
-      // go to that floor
       elevator.goToFloor(floorNum);
+    });
+
+    // when passing a floor ...
+    elevator.on("passing_floor", function(floorNum, direction) {
+    });
+
+    // when elevator is stopped at a floor ...
+    elevator.on("stopped_at_floor", function(floorNum) {
     });
 
     // when elevator is idle ...
     elevator.on("idle", function () {
-      // go back to floor 0 where most people may be making requests
       elevator.goToFloor(0);
     });
 
   },
   update: function(dt, elevators, floors) {
+    console.log(`destinationQueue: ${elevators[0].destinationQueue}`);
   }
 }
