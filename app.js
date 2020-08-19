58298d8edc1fea33b6adda68ade9954eb11d3ade
Brian Larson <brianl@scgpr.com>
2020-08-18 21:01:04 -0500

    finish challenge 2

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 {
   init: function(elevators, floors) {
+
     // use first and only elevator
     const elevator = elevators[0];
+
+    // when a passenger pushes a floor button ...
+    elevator.on("floor_button_pressed", function(floorNum) {
+      // log which button
+      console.log(`floor ${floorNum} button pressed`);
+      // go to that floor
+      elevator.goToFloor(floorNum);
+    });
+
     // when elevator is idle ...
     elevator.on("idle", function () {
-      // loop through floors picking up cute little bathroom sign folx willy nilly
-      for (let i = 0; i < floors.length; i++) {
-        elevator.goToFloor(i);
-      }
+      // go back to floor 0 where most people may be making requests
+      elevator.goToFloor(0);
     });
+
   },
   update: function(dt, elevators, floors) {
   }
